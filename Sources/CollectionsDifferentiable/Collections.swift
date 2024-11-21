2d4e71d79b6aab744d7f9ec2a8c5b9a6fa02534f
Jaap Wijnen <jaap@passivelogic.com>
2024-11-21 10:40:57 +0100

    feat: create initial package

---
+++ Sources/CollectionsDifferentiable/Collections.swift
@@ -0,0 +1,4 @@
+// A module that re-exports Swift Collections API and the Differentiable extensions to the Swift Collections API
+@_exported import Collections
+// Swift Collection modules with Differentiable extensions
+@_exported import OrderedCollectionsDifferentiable
