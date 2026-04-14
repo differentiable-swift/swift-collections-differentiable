80ebcb396c77b568087be3e98488a6cd6047ce5e
Jaap Wijnen <wijnen.jaap@gmail.com>
2026-04-14 13:51:39 +0200

    feat: support 26.4 OSs

--- Package.swift
+++ Package.swift
@@ -1,9 +1,13 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 
 import PackageDescription
 
 let package = Package(
     name: "swift-collections-differentiable",
+    platforms: [
+        .macOS("26.0"),
+        .iOS("26.0"),
+    ],
     products: [
         .library(
             name: "CollectionsDifferentiable",
@@ -16,6 +20,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.4"),
+        .package(url: "https://github.com/differentiable-swift/swift-differentiation.git", from: "2.0.0"),
     ],
     targets: [
         .target(
@@ -29,6 +34,7 @@ let package = Package(
             name: "OrderedCollectionsDifferentiable",
             dependencies: [
                 .product(name: "OrderedCollections", package: "swift-collections"),
+                .product(name: "Differentiation", package: "swift-differentiation"),
             ]
         ),
         .testTarget(
